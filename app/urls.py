9eff05107df7587eae94aed807eb5c4aa2df612c
umid <umidjonuchqunugli@gmail.com>
2023-07-19 15:12:00 +0500

    many chenges

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app.views import IndexView
+from app.views import IndexView, addproduct
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView, name='index'),
+    path('addproduct/', addproduct, name='add-product')
 ]
--- app/views.py
+++ app/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class IndexView(TemplateView):
-    template_name = 'app/index.html'
+def IndexView(request):
+    return render(request=request,
+                  template_name='app/index.html')
+
+def addproduct(request):
+    return render(request=request,
+                  template_name='app/add_product.html')
